a99ac77c2a9941ca72f3eba94c4517af6c475b1b
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-05-18 17:47:10 +0300

    Updated files

--- get_av.c
+++ get_av.c
@@ -1,33 +1,4 @@
 #include "main.h"
-
-int _strtok(int argc, char *argv[])
-{
-	char *str1, *str2, *token, *subtoken;
-	char *saveptr1, *saveptr2;
-
-	if (argc != 4) {
-		fprintf(stderr, "Usage: %s string delim subdelim\n",
-				argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	for (int j = 1, str1 = argv[1]; ; j++, str1 = NULL) {
-		token = strtok_r(str1, argv[2], &saveptr1);
-		if (token == NULL)
-			break;
-		printf("%d: %s\n", j, token);
-
-		for (str2 = token; ; str2 = NULL) {
-			subtoken = strtok_r(str2, argv[3], &saveptr2);
-			if (subtoken == NULL)
-				break;
-			printf(" --> %s\n", subtoken);
-		}
-	}
-
-	exit(EXIT_SUCCESS);
-}
-
 char **get_av(char *line) {
 	int idx = 0;
 
@@ -40,11 +11,11 @@ char **get_av(char *line) {
 		exit(EXIT_FAILURE);
 	}
 
-	token = _strtok(line, delim);
+	token = strtok(line, delim);
 	while (token != NULL) {
 		tokenList[idx] = token;
 		idx++;
-		token = _strtok(NULL, delim);
+		token = strtok(NULL, delim);
 	}
 
 	tokenList[idx] = NULL;
--- main.c
+++ main.c
@@ -1,29 +1,75 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/wait.h>
 
-int waiting = 0;
-
-void handler(int sig) {
+int main(void) {
+  ssize_t stat;
   int status;
-  waitpid(-1, &status, WNOHANG);
-}
-
-int main() {
-  int inte = 1, status, count;
   char *prompt;
+  size_t len = 0;
+  char *line = NULL;
+  char **args;
   pid_t pid;
 
-  signal(SIGCHLD, handler);
-  pid = fork();
   prompt = "($)";
-  do {
-    if (waiting == 1) {
-      while (wait(NULL) != pid)
-        ;
-    }
-    printf("%s", prompt);
-    status = execute(get_args(get_line()), pid);
-  } while (status);
 
-  return 0;
+  while (stat) {
+    if (isatty(0)) {
+      if (pid != 0) {
+        wait();
+      }
+      write(1, prompt, 3);
+      stat = getline(&line, &len, stdin);
+    }
+    if (feof(stdin))
+      return (0);
+    args = get_av(line);
+
+    if (args[0] == NULL) {
+      ;
+      continue;
+    }
+    if (feof(stdin))
+      exit(EXIT_SUCCESS);
+    pid = fork();
+    status = execute(args, pid);
+    fflush(stdout);
+  }
+  return ((int)stat);
 }
 
+int execute(char **args, pid_t pid) {
+  char *envs[] = {NULL};
+
+  if (args[0] == NULL) {
+    return 1;
+  }
+
+  if (strcmp(args[0], "exit") == 0) {
+    return 0;
+  }
+
+  if (strcmp(args[0], "cd") == 0) {
+    if (args[1] == NULL) {
+      fprintf(stderr, "simple shell: expected argument to \"cd\"\n");
+    } else {
+      if (chdir(args[1]) != 0) {
+        perror("simple shell");
+      }
+    }
+
+    return 1;
+  }
+
+  if (pid == 0) {
+
+    if (execve(args[0], args, envs) == -1)
+      perror("basic shell");
+
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    perror("basic shell");
+  }
+
+  return 1;
+}
