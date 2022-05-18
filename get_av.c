062af25d8eb84ccb2edccacc7e1ef10afbb2a361
Daniel Kigen Buturu <dbuturu@gmail.com>
2022-05-18 18:06:19 +0300

    Updated files

--- get_av.c
+++ get_av.c
@@ -1,24 +1,28 @@
 #include "main.h"
-char **get_av(char *line) {
+
+char **get_av(char *line)
+{
 	int idx = 0;
 
 	char delim[] = " \t\r\n";
 	char **tokenList = malloc(MAXTOKENS * sizeof(char *));
 	char *token;
 
-	if (!tokenList) {
+	if (!tokenList)
+	{
 		perror("simple shell: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
 	token = strtok(line, delim);
-	while (token != NULL) {
+	while (token != NULL)
+	{
 		tokenList[idx] = token;
 		idx++;
 		token = strtok(NULL, delim);
 	}
 
 	tokenList[idx] = NULL;
-	return tokenList;
+	return (tokenList);
 }
 
--- main.c
+++ main.c
@@ -2,74 +2,80 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-int main(void) {
-  ssize_t stat;
-  int status;
-  char *prompt;
-  size_t len = 0;
-  char *line = NULL;
-  char **args;
-  pid_t pid;
+int main(void)
+{
+	ssize_t stat;
+	int status;
+	char *prompt;
+	size_t len = 0;
+	char *line = NULL;
+	char **args;
+	pid_t pid;
 
-  prompt = "($)";
+	prompt = "($)";
 
-  while (stat) {
-    if (isatty(0)) {
-      if (pid != 0) {
-        wait();
-      }
-      write(1, prompt, 3);
-      stat = getline(&line, &len, stdin);
-    }
-    if (feof(stdin))
-      return (0);
-    args = get_av(line);
+	while (stat)
+	{
+		if (isatty(0))
+		{
+			if (pid != 0)
+			{
+				wait();
+			}
+			write(1, prompt, 3);
+			stat = getline(&line, &len, stdin);
+		}
+		if (feof(stdin))
+			return (0);
+		args = get_av(line);
 
-    if (args[0] == NULL) {
-      ;
-      continue;
-    }
-    if (feof(stdin))
-      exit(EXIT_SUCCESS);
-    pid = fork();
-    status = execute(args, pid);
-    fflush(stdout);
-  }
-  return ((int)stat);
+		if (args[0] == NULL)
+			continue;
+		if (feof(stdin))
+			exit(EXIT_SUCCESS);
+		pid = fork();
+		status = execute(args, pid);
+		fflush(stdout);
+	}
+	return ((int)stat);
 }
 
-int execute(char **args, pid_t pid) {
-  char *envs[] = {NULL};
+int execute(char **args, pid_t pid)
+{
+	char *envs[] = {NULL};
 
-  if (args[0] == NULL) {
-    return 1;
-  }
+	if (args[0] == NULL)
+		return (1);
 
-  if (strcmp(args[0], "exit") == 0) {
-    return 0;
-  }
+	if (strcmp(args[0], "exit") == 0)
+		return (0);
 
-  if (strcmp(args[0], "cd") == 0) {
-    if (args[1] == NULL) {
-      fprintf(stderr, "simple shell: expected argument to \"cd\"\n");
-    } else {
-      if (chdir(args[1]) != 0) {
-        perror("simple shell");
-      }
-    }
+	if (strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] == NULL)
+		{
+			fprintf(stderr, "simple shell: expected argument to \"cd\"\n");
+		} else
+		{
+			if (chdir(args[1]) != 0)
+				perror("simple shell");
+		}
 
-    return 1;
-  }
+		return (1);
+	}
 
-  if (pid == 0) {
+	if (pid == 0)
+	{
 
-    if (execve(args[0], args, envs) == -1)
-      perror("basic shell");
+		if (execve(args[0], args, envs) == -1)
+			perror("basic shell");
 
-    exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    perror("basic shell");
-  }
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+	{
+		perror("basic shell");
+	}
 
-  return 1;
+	return (1);
 }
+
